6105512f6898d8d50f05715edb3e6b5b1d02235d
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-08 11:16:54 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      maxChars: 280
+    };
   }
-
+  handleInputChange = () => {
+    this.setState( preState => {
+      return{
+        maxChars: preState.maxChars -1
+       }
+     }
+    )
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message"onChange={this.handleInputChange} />
+        <p>{this.state.maxChars}</p>
       </div>
     );
   }
